030c8fbf0085e7e95f0dbb669bbd9cc23222f9c4
Havunen <sampo.kivisto@live.fi>
2024-02-18 15:48:36 +0200

    merge Remove ReDoc ghosts fix

---
+++ src/DotSwashbuckle.AspNetCore.Redoc/RedocMiddleware.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DotSwashbuckle.AspNetCore.Redoc
+{
+    public class RedocMiddleware
+    {
+        private const string EmbeddedFileNamespace = "DotSwashbuckle.AspNetCore.Redoc.node_modules.redoc.bundles";
+
+        private readonly RedocOptions _options;
+        private readonly StaticFileMiddleware _staticFileMiddleware;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public RedocMiddleware(
+            RequestDelegate next,
+            IWebHostEnvironment hostingEnv,
+            ILoggerFactory loggerFactory,
+            RedocOptions options)
+        {
+            _options = options ?? new RedocOptions();
+
+            _staticFileMiddleware = CreateStaticFileMiddleware(next, hostingEnv, loggerFactory, options);
+
+            _jsonSerializerOptions = new JsonSerializerOptions();
+            _jsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            _jsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            _jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, false));
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var httpMethod = httpContext.Request.Method;
+            var path = httpContext.Request.Path.Value;
+
+            // If the RoutePrefix is requested (with or without trailing slash), redirect to index URL
+            if (httpMethod == "GET" && Regex.IsMatch(path, $"^/?{Regex.Escape(_options.RoutePrefix)}/?$",  RegexOptions.IgnoreCase))
+            {
+                // Use relative redirect to support proxy environments
+                var relativeIndexUrl = string.IsNullOrEmpty(path) || path.EndsWith("/")
+                    ? "index.html"
+                    : $"{path.Split('/').Last()}/index.html";
+
+                RespondWithRedirect(httpContext.Response, relativeIndexUrl);
+                return;
+            }
+
+            if (httpMethod == "GET" && Regex.IsMatch(path, $"/{_options.RoutePrefix}/?index.html",  RegexOptions.IgnoreCase))
+            {
+                await RespondWithIndexHtml(httpContext.Response);
+                return;
+            }
+
+            await _staticFileMiddleware.Invoke(httpContext);
+        }
+
+        private StaticFileMiddleware CreateStaticFileMiddleware(
+            RequestDelegate next,
+            IWebHostEnvironment hostingEnv,
+            ILoggerFactory loggerFactory,
+            RedocOptions options)
+        {
+            var staticFileOptions = new StaticFileOptions
+            {
+                RequestPath = string.IsNullOrEmpty(options.RoutePrefix) ? string.Empty : $"/{options.RoutePrefix}",
+                FileProvider = new EmbeddedFileProvider(typeof(RedocMiddleware).GetTypeInfo().Assembly, EmbeddedFileNamespace),
+            };
+
+            return new StaticFileMiddleware(next, hostingEnv, Options.Create(staticFileOptions), loggerFactory);
+        }
+
+        private void RespondWithRedirect(HttpResponse response, string location)
+        {
+            response.StatusCode = 301;
+            response.Headers["Location"] = location;
+        }
+
+        private async Task RespondWithIndexHtml(HttpResponse response)
+        {
+            response.StatusCode = 200;
+            response.ContentType = "text/html";
+
+            using (var stream = _options.IndexStream())
+            {
+                // Inject arguments before writing to response
+                var htmlBuilder = new StringBuilder(new StreamReader(stream).ReadToEnd());
+                foreach (var entry in GetIndexArguments())
+                {
+                    htmlBuilder.Replace(entry.Key, entry.Value);
+                }
+
+                await response.WriteAsync(htmlBuilder.ToString(), Encoding.UTF8);
+            }
+        }
+
+        private IDictionary<string, string> GetIndexArguments()
+        {
+            return new Dictionary<string, string>(StringComparer.Ordinal)
+            {
+                { "%(DocumentTitle)", _options.DocumentTitle },
+                { "%(HeadContent)", _options.HeadContent },
+                { "%(SpecUrl)", _options.SpecUrl },
+                { "%(ConfigObject)", JsonSerializer.Serialize(_options.ConfigObject, _jsonSerializerOptions) }
+            };
+        }
+    }
+}
