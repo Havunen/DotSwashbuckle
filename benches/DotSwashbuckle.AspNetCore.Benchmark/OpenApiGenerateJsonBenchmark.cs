ef3bab75a6a9e7b2d3bfa06e27a1c31a24752c53
Havunen <sampo.kivisto@live.fi>
2024-02-18 14:54:54 +0200

    split benchmarks impl to logic project

---
+++ benches/DotSwashbuckle.AspNetCore.Benchmark/OpenApiGenerateJsonBenchmark.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using DotSwashbuckle.AspNetCore.Benchmark.Logic;
+using System.Runtime.Loader;
+
+namespace DotSwashbuckle.AspNetCore.Benchmark
+{
+    public class OpenApiGenerateJsonBenchmark
+    {
+        private readonly Dictionary<string, IServiceProvider> assemblyProviders = new(StringComparer.Ordinal);
+
+        private string[] assemblies = new string[] { "Basic.dll" };
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            assemblyProviders.Add(
+                "Basic.dll",
+                AssemblyServiceProvider.GetServiceProvider(
+                    AssemblyLoadContext.Default.LoadFromAssemblyPath(
+                        Path.Combine(Directory.GetCurrentDirectory(), "Basic.dll")
+                    )
+                )
+            );
+        }
+
+        [Benchmark]
+        public string DotSwashbuckleOpenApiV2()
+        {
+            return DotSwashbuckleGenerator.CreateSwaggerDoc(
+                assemblyProviders["Basic.dll"],
+                false,
+                true
+            );
+        }
+    }
+}
