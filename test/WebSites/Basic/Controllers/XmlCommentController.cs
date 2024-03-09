42112e15720115d8f8afe1e6ccb48a57bd1617cc
Havunen <sampo.kivisto@live.fi>
2024-03-09 09:34:49 +0200

    added a test for xml comment generation

---
+++ test/WebSites/Basic/Controllers/XmlCommentController.cs
@@ -0,0 +1,38 @@
+﻿using DotSwashbuckle.AspNetCore.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Basic.Controllers
+{
+    [Route("/xmlComments")]
+    public class XmlCommentController
+    {
+        /// <summary>
+        /// Initialize request to api.
+        /// </summary>
+        /// <param name="request">Id of the request returned by POST.</param>
+        /// <param name="timeStamp">the moment.</param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [SwaggerResponse(201, "The created status.", typeof(MyRequest))]
+        [SwaggerResponse(400, "An error occurred. Message is returned.", typeof(string))]
+        [SwaggerResponse(401, "Unauthorized.", typeof(string))]
+        [SwaggerResponse(404, "Not found.", typeof(string))]
+        [SwaggerResponse(500, "Unexpected exception while communicating with API.", typeof(string))]
+        [HttpPost("xmlPost")]
+        public async Task<ActionResult<MyRequest>> Post(
+            [FromBody] MyRequest request,
+            [FromQuery] string timeStamp,
+            CancellationToken token
+        )
+        {
+            return null;
+        }
+
+        public class MyRequest
+        {
+            public string Id { get; set; }
+        }
+    }
+}
